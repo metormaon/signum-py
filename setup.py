7c9dcad8e80153d72ba8bf5b5c240b0463f84604
Noam Rotem <noam.rotem@thomsonreuters.com>
2020-05-14 00:16:19 +0300

    #8 - pointing setup.py to source

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/metormaon/signum-py",
-    packages=setuptools.find_packages(),
+    package_dir={'': 'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
