de199e32ac119bd28b75907001ef75d9274d7023
Noam Rotem <noam.rotem@thomsonreuters.com>
2020-05-14 00:45:03 +0300

    #8 - replacing setuptools with distutils

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
-import setuptools
+from distutils.core import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
+setup(
     name="signum-py", # Replace with your own username
     version="0.0.1",
-    author="Moran Mahabi & Noam Rotem",
+    author="Moran Mahabi, Noam Rotem",
+    author_email="moran.mahabi@gmail.com, metormaon@gmail.com",
     description="Sign-in tools for Python servers",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/metormaon/signum-py",
-    package_dir={'': 'src'},
+    package_dir={'signum': 'src'},
+    packages=['signum'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    ]
 )
