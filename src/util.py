1fa3ab56ebd6d75345c8714c8871c221bfd3fc7f
Noam Rotem <noam.rotem@thomsonreuters.com>
2020-05-16 20:30:14 +0300

    #4 - adding utility for verifying zeros in hashcash

--- src/util.py
+++ src/util.py
@@ -1,4 +1,7 @@
+import binascii
 import secrets
+import hashlib
+import sys
 
 
 def generate_random_base_64(length_in_bytes: object = 20) -> str:
@@ -8,4 +11,21 @@ def generate_random_base_64(length_in_bytes: object = 20) -> str:
     :return: a random BASE64 string
     """
     return secrets.token_urlsafe(length_in_bytes)
+
+
+def validate_hashcash_zeros(hashcash: bytes, zero_count: int) -> bool:
+    try:
+        hashing = hashlib.sha1()
+        hashing.update(hashcash)
+
+        hash_result = hashing.hexdigest()
+
+        # convert from hex to binary and keep leading zeros
+        binary_string = (bin(int(hash_result, 16))[2:]).zfill(len(hash_result) * 4)
+
+        return binary_string.startswith('0' * zero_count)
+
+    except Exception as e:
+        print(e)
+        return False
 
