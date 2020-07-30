4efce4a8a140961e43b37cc51888967bf1194403
Noam Rotem <noam.rotem@thomsonreuters.com>
2020-07-31 02:45:28 +0300

    #3, #13 - trying to solve build issues - from src is replaced with from signum

--- test/test_authentication_validator.py
+++ test/test_authentication_validator.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 from unittest.mock import Mock
 
 from src.authentication_validator import AuthenticationValidator
-from src.password_repository import PasswordRepository
 from src.state import StateEncryptor
 
 
